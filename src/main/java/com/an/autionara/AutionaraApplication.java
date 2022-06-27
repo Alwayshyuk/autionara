f7acb09aed04bf6425b4de54f0d4d000c8e3b600
본인 <never5686@naver.com>
2022-06-27 20:41:19 +0900

    테스트

---
+++ src/main/java/com/an/autionara/AutionaraApplication.java
@@ -0,0 +1,13 @@
+package com.an.autionara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutionaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutionaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/an/autionara/AutionaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.an.autionara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutionaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
